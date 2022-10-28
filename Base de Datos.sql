74f2240fd4c1a3a89e97fa97483c3a42a46d1a2d
Joel Ricaño <joel.ricanorodriguez@utxicotepec.edu.mx>
2022-10-27 23:13:56 -0500

    Base de Datos y Modelo Entidad-Relacion

---
+++ Base de Datos.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: videojuego
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `jugadores`
+--
+
+DROP TABLE IF EXISTS `jugadores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jugadores` (
+  `ID` int unsigned NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Puntaje` int NOT NULL,
+  `Posicion` int NOT NULL,
+  `Alias` varchar(45) NOT NULL,
+  `Birthdate` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jugadores`
+--
+
+LOCK TABLES `jugadores` WRITE;
+/*!40000 ALTER TABLE `jugadores` DISABLE KEYS */;
+INSERT INTO `jugadores` VALUES (1,'Player1',5000,1,'Elchicarcas','2002-06-22 00:00:00'),(2,'Player2',1234,5,'Ciber1','2022-03-22 00:00:00'),(3,'Player3',3456,3,'CocoCabra','2012-05-22 00:00:00'),(4,'Player4',2345,4,'Zhongli','2013-06-22 00:00:00'),(5,'Player5',4000,2,'YamiRalph','2004-09-22 00:00:00');
+/*!40000 ALTER TABLE `jugadores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `niveles`
+--
+
+DROP TABLE IF EXISTS `niveles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `niveles` (
+  `ID` int NOT NULL,
+  `Score` int NOT NULL DEFAULT '1',
+  `Date` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `niveles`
+--
+
+LOCK TABLES `niveles` WRITE;
+/*!40000 ALTER TABLE `niveles` DISABLE KEYS */;
+INSERT INTO `niveles` VALUES (1,5000,'2024-09-22 00:00:00'),(2,1234,'2024-09-22 00:00:00'),(3,3456,'2024-09-22 00:00:00'),(4,2345,'2024-09-22 00:00:00'),(5,4000,'2024-09-22 00:00:00');
+/*!40000 ALTER TABLE `niveles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puntaje`
+--
+
+DROP TABLE IF EXISTS `puntaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `puntaje` (
+  `ID` int NOT NULL,
+  `Player` int NOT NULL,
+  `Ranking` int NOT NULL,
+  `Update` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`,`Player`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `puntaje`
+--
+
+LOCK TABLES `puntaje` WRITE;
+/*!40000 ALTER TABLE `puntaje` DISABLE KEYS */;
+/*!40000 ALTER TABLE `puntaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'videojuego'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-27 23:04:55
