2a8c4b35124d37dc33dd78415df600fe429214e0
Joel Ricaño Rodríguez <75385420+Joel-RR@users.noreply.github.com>
2022-11-15 16:15:16 -0600

    Base de Datos

--- Base de Datos.sql
+++ Base de Datos.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `videjuego_crj` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `videjuego_crj`;
+CREATE DATABASE  IF NOT EXISTS `videjuego` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `videjuego`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: videjuego_crj
+-- Host: localhost    Database: videjuego
 -- ------------------------------------------------------
 -- Server version	8.0.30
 
@@ -25,14 +25,21 @@ DROP TABLE IF EXISTS `jugadores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `jugadores` (
-  `ID_jugador` int unsigned NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(45) NOT NULL,
-  `nombre_jugador` varchar(45) NOT NULL,
-  `Birthdate` datetime DEFAULT NULL,
+  `id_jugador` int NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellidos` varchar(45) NOT NULL,
+  `alias` varchar(45) NOT NULL,
+  `cumpleaños` varchar(45) NOT NULL,
+  `primer_login` varchar(45) NOT NULL,
   `correo` varchar(45) NOT NULL,
-  `firts_login` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID_jugador`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_partida` int NOT NULL,
+  `id_personaje` int NOT NULL,
+  PRIMARY KEY (`id_jugador`),
+  KEY `fk_01_idx` (`id_partida`),
+  KEY `fk_02_idx` (`id_personaje`),
+  CONSTRAINT `fk_01` FOREIGN KEY (`id_partida`) REFERENCES `partidas` (`id_partida`),
+  CONSTRAINT `fk_02` FOREIGN KEY (`id_personaje`) REFERENCES `personaje` (`id_personaje`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,65 +48,69 @@ CREATE TABLE `jugadores` (
 
 LOCK TABLES `jugadores` WRITE;
 /*!40000 ALTER TABLE `jugadores` DISABLE KEYS */;
-INSERT INTO `jugadores` VALUES (1,'Player1','Elchicarcas','2002-06-22 00:00:00','Elchicarcas@gmail.com','17/09/2022'),(2,'Player2','Ciber1','2022-03-22 00:00:00','Ciber1@gmail.com','18/09/2022'),(3,'Player3','CocoCabra','2012-05-22 00:00:00','CocoCabra@gmail.com','19/09/2022'),(4,'Player4','Zhongli','2013-06-22 00:00:00','Zhongli@gmail.com','20/09/2022'),(5,'Player5','YamiRalph','2004-09-22 00:00:00','YamiRalph@gmail.com','21/09/2022');
+INSERT INTO `jugadores` VALUES (1,'Juan','Sanchez','Elchicarcas','2002-06-22 00:00:00','17/09/2022','Elchicarcas@gmail.com',12,12),(2,'Luis','Gomez','Ciber1','2022-03-22 00:00:00','18/09/2022','Ciber1@gmail.com',13,12),(3,'Cesar','Hernandez','CocoCabra','2012-05-22 00:00:00','19/09/2022','CocoCabra@gmail.com',14,12),(4,'Rafael','Ambar','Zhongli','2013-06-22 00:00:00','20/09/2022','Zhongli@gmail.com',15,12),(5,'Joel','Ricaño','YamiRalph','2004-09-22 00:00:00','21/09/2022','YamiRalph@gmail.com',16,12);
 /*!40000 ALTER TABLE `jugadores` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `niveles`
+-- Table structure for table `partidas`
 --
 
-DROP TABLE IF EXISTS `niveles`;
+DROP TABLE IF EXISTS `partidas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `niveles` (
-  `ID` int NOT NULL,
-  `nombre_Nivel` varchar(45) NOT NULL,
-  `descripcion` varchar(45) NOT NULL,
-  `IDSS` int unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`ID`),
-  KEY `fk_34_idx` (`IDSS`),
-  CONSTRAINT `fk_34` FOREIGN KEY (`IDSS`) REFERENCES `jugadores` (`ID_jugador`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `niveles`
---
-
-LOCK TABLES `niveles` WRITE;
-/*!40000 ALTER TABLE `niveles` DISABLE KEYS */;
-INSERT INTO `niveles` VALUES (1,'Mundo 1-1','Area de TIC.',1),(2,'Mundo 1-2','Area de Enfermeria.',2),(3,'Mundo 1-3','Area de Mecanica Automotris.',3),(4,'Mundo 1-4','Servicios Escolares.',4),(5,'Mundo 1-5','Bosque Tenebroso',5);
-/*!40000 ALTER TABLE `niveles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `puntaje`
---
-
-DROP TABLE IF EXISTS `puntaje`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `puntaje` (
-  `id_puntaje` int NOT NULL,
-  `puntajes` int NOT NULL,
-  `partida` int NOT NULL,
-  `posicion` int NOT NULL,
-  PRIMARY KEY (`id_puntaje`),
-  KEY `for_1_idx` (`partida`),
-  CONSTRAINT `for_1` FOREIGN KEY (`partida`) REFERENCES `niveles` (`ID`)
+CREATE TABLE `partidas` (
+  `id_partida` int NOT NULL,
+  `nivel` varchar(45) NOT NULL,
+  `score` varchar(45) NOT NULL,
+  `id_personaje` int NOT NULL,
+  PRIMARY KEY (`id_partida`),
+  KEY `fk_02_idx` (`id_personaje`),
+  CONSTRAINT `fk_03` FOREIGN KEY (`id_personaje`) REFERENCES `personaje` (`id_personaje`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `puntaje`
+-- Dumping data for table `partidas`
 --
 
-LOCK TABLES `puntaje` WRITE;
-/*!40000 ALTER TABLE `puntaje` DISABLE KEYS */;
-INSERT INTO `puntaje` VALUES (1,5000,1,1),(2,4000,2,2),(3,3000,3,3),(4,2000,4,4),(5,1000,5,5);
-/*!40000 ALTER TABLE `puntaje` ENABLE KEYS */;
+LOCK TABLES `partidas` WRITE;
+/*!40000 ALTER TABLE `partidas` DISABLE KEYS */;
+INSERT INTO `partidas` VALUES (12,'Mundo 1-1','400',12),(13,'Mundo 1-2','200',12),(14,'Mundo 1-3','150',12),(15,'Mundo 1-4','40',12),(16,'Mundo 1-5','100',12);
+/*!40000 ALTER TABLE `partidas` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `personaje`
+--
+
+DROP TABLE IF EXISTS `personaje`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personaje` (
+  `id_personaje` int NOT NULL,
+  `nombre_personaje` varchar(45) NOT NULL,
+  `descripcion` varchar(1000) NOT NULL,
+  `arma` varchar(45) NOT NULL,
+  `municion` varchar(45) NOT NULL,
+  `vidas` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_personaje`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personaje`
+--
+
+LOCK TABLES `personaje` WRITE;
+/*!40000 ALTER TABLE `personaje` DISABLE KEYS */;
+INSERT INTO `personaje` VALUES (12,'Willian Afton','Un Vigilante de seguiridad de la universidad, valiente, herorico, honesto.','Pistola.','100','5');
+/*!40000 ALTER TABLE `personaje` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'videjuego'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -110,5 +121,5 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-14 16:19:23
+-- Dump completed on 2022-11-15 16:12:49
 
